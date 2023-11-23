62bfb34043ba41d3ba6f9bb619a389620c98da56
Raendhanaiv69 <raendhanaiv@telkomuniversity.ac.id>
2023-11-23 11:54:00 +0700

    buddies

---
+++ tubes-buddies/resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        {{-- style --}}
+        <link rel="stylesheet" href="resources/css/lp.css">
+    </head>
+    <body>
+
+        <div class="landing">
+            @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/dashboard') }}">Dashboard</a>
+                    @else
+                        <a href="welcome.blade.php">Home</a>
+                        <a href="course.blade.php">course</a>
+                        <a href="{{ route('login') }}" >Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+    </body>
+</html>
